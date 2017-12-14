83540813a16e5cd975cce66323e417167b99dd9f
Xianyi555 <xianyi555@gmail.com>
2017-12-13 23:50:35 -0800

    completed sprint-1

--- server.js
+++ server.js
@@ -21,60 +21,9 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-////////////////////
-//  DATA
-///////////////////
-
-app.get('/api/books', function (req, res) {
-  // send all books as JSON response
-  db.Book.find(function(err, books){
-    if (err) {
-      console.log("index error: " + err);
-      res.sendStatus(500);
-    }
-    res.json(books);
-  });
-});
-
-// var books = [
-//   {
-//     _id: 15,
-//     title: "The Four Hour Workweek",
-//     author: "Tim Ferriss",
-//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-//     release_date: "April 1, 2007"
-//   },
-//   {
-//     _id: 16,
-//     title: "Of Mice and Men",
-//     author: "John Steinbeck",
-//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-//     release_date: "Unknown 1937"
-//   },
-//   {
-//     _id: 17,
-//     title: "Romeo and Juliet",
-//     author: "William Shakespeare",
-//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-//     release_date: "Unknown 1597"
-//   }
-// ];
-
-
-var newBookUUID = 18;
-
-
-
-
-
-
-
 ////////////////////
 //  ROUTES
 ///////////////////
-
-
 
 
 // define a root route: localhost:3000/
@@ -83,67 +32,51 @@ app.get('/', function (req, res) {
 });
 
 // get all books
+
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, booksFromDb){
+    if (err) {
+      console.log("index error: " + err);
+      res.sendStatus(500);
+    }
+    res.json(booksFromDb);
+  });
 });
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
   // find one book by its id
-  console.log('books show', req.params);
-  for(var i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
+  var bookId = req.params.id;
+  db.Book.findOne({ _id: bookId}, function(err, foundId){
+    res.json(foundId);
+  });
 });
 
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  newBook._id = newBookUUID++;
-  books.push(newBook);
-  res.json(newBook);
+   db.Book.create(req.body, function(err, data){
+     res.json(data)
+   });
 });
 
 // update book
 app.put('/api/books/:id', function(req,res){
-// get book id from url params (`req.params`)
-  console.log('books update', req.params);
   var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var updateBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
+  db.Book.findByIdAndUpdate(bookId,{ $set: { title: req.body.title, author: req.body.author}}, function(err, newBook){
+    res.json(newBook); 
   });
-  console.log('updating book with index', deleteBookIndex);
-  var bookToUpdate = books[deleteBookIndex];
-  books.splice(updateBookIndex, 1, req.params);
-  res.json(req.params);
 });
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
-  // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var deleteBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('deleting book with index', deleteBookIndex);
-  var bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
+   var bookId = req.params.id; 
+   db.Book.findByIdAndRemove( {_id: bookId}, function(err, deletedId){
+    res.json(deletedId);
+   });
+  
 });
-
-
-
-
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Book app listening at http://localhost:3000/');
